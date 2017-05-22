28666567a32f6489ec2405502d4c7b796f3c37a4
yzbx <youdaoyzbx@163.com>
2017-05-22 20:32:38 +0800

    zzz

--- example007-tflearn/yzbx/ihead.py
+++ example007-tflearn/yzbx/ihead.py
@@ -19,6 +19,7 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
+import matplotlib.pyplot as plt
 
 # Data loading and preprocessing
 import tflearn.datasets.mnist as mnist
@@ -42,15 +43,3 @@ network = fully_connected(network, n_units=10, activation='softmax')
 network = regression(network, optimizer='adam', learning_rate=0.01,
                      loss='categorical_crossentropy', name='target')
 
-# Training
-
-try:
-    model = tflearn.DNN(network, tensorboard_verbose=0,
-        checkpoint_path='/tmp/tflearn/checkpoint_path',
-        best_checkpoint_path='/tmp/tflearn/best_checkpoint_path')
-    model.fit({'input': X}, {'target': Y}, n_epoch=20,
-               validation_set=({'input': testX}, {'target': testY}),
-               snapshot_step=100, show_metric=True, run_id='yzbx_convnet_mnist')
-except KeyboardInterrupt:
-    print('save model '+'.'*60);
-    model.save('/tmp/tflearn/convnet_mnist_ctrl_c_model')
